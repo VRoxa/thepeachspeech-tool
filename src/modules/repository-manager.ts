5ca4b05f445fd8203cfcb150256de5f3c4c7cf72
VRoxa <victor.roxa.r@gmail.com>
2022-02-22 13:03:29 +0100

    Implement repository manager

---
+++ src/modules/repository-manager.ts
@@ -0,0 +1,37 @@
+import { Clone, FetchOptions, Remote, Repository } from "nodegit";
+import { getCredentialsCallbacks } from "./credentials";
+
+export class RepositoryManager {
+
+  private repository!: Repository;
+
+  clone = async (repositoryUrl: string, cloningPath: string) => {
+    this.repository = await Clone.clone(cloningPath, repositoryUrl, { 
+      bare: 0,
+      fetchOpts: {
+        callbacks: getCredentialsCallbacks()
+      }
+    });
+  }
+
+  fetch = async () => {
+      // Fetch any change from the remote 'master' branch
+    const remote = await Remote.lookup(this.repository, 'origin');
+    
+    const options: FetchOptions = {
+      downloadTags: 1,
+      prune: 1,
+      updateFetchhead: 1,
+      callbacks: getCredentialsCallbacks()
+    };
+
+    await remote.fetch(['+refs/*:refs/*'], options, 'Automated');
+  }
+
+  push = async () => {
+    const remote = await Remote.lookup(this.repository, 'origin');
+    await remote.push(['refs/heads/master:refs/heads/master'], {
+      callbacks: getCredentialsCallbacks()
+    });
+  }
+}
