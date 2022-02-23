c675b6f070b445f8c0eb49b02663d52834ee6b27
VRoxa <victor.roxa.r@gmail.com>
2022-02-24 00:07:29 +0100

    Implement generic Select with inner rendering per element

--- src/components/common/SelectText.tsx
+++ src/components/common/SelectText.tsx
@@ -3,15 +3,13 @@ import React, { useState } from "react"
 import { useArrows } from "../../hooks/use-arrows";
 import { primary } from "../../styles/colors";
 
-export const Select = <T extends {}>({ elements, render, onSelect }: SelectProps<T>) => {
+export const SelectText = ({ elements: options, onSelect }: SelectTextProps) => {
   const [selected, setSelected] = useState(0);
-  const { length } = elements;
+  const { length } = options;
 
   useInput((_, key) => {
     if (key.return) {
-      // On select callback is defined
-      !!onSelect &&
-      onSelect(elements[selected], selected);
+      onSelect(selected);
     }
   });
 
@@ -19,22 +17,25 @@ export const Select = <T extends {}>({ elements, render, onSelect }: SelectProps
     onUp: () => setSelected((selected + length - 1) % length),
     onDown: () => setSelected((selected + 1) % length)
   });
-  
+
   return (
     <Box flexDirection="column">
-      {elements.map((element, i) => 
+      {options.map((option, i) => (
         <Box marginY={0.5} key={i + 'opt'}>
-          {render(element, i === selected, i)}
+          <Option
+            value={option}
+            index={i}
+            selected={i === selected}
+          />
         </Box>
-      )}
+      ))}
     </Box>
   );
 }
 
-export interface SelectProps<T extends {}> {
-  elements: T[];
-  render: (element: T, selected: boolean, index: number) => JSX.Element;
-  onSelect?: (element: T, index: number) => void;
+export interface SelectTextProps {
+  elements: string[];
+  onSelect: (optionIndex: number) => void;
 }
 
 const Option = ({ value, selected }: OptionProps) => {
